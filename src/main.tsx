b0763af085ebf5eccd6aa014cdc0719ec08e045c
Diwanshu <saidiwanshu1880@gmail.com>
2024-03-05 15:52:21 +0530

    New Dist

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// @ts-nocheck
+
+import ReactDOM from "react-dom";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.Chatbot = {
+  mount: function (el) {
+    ReactDOM.render(<App />, el);
+  },
+};
